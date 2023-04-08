389c283ab3dd9341d5f45faa9b180836b2d7aa4d
Kavi2101sh <36762897+Kavi2101sh@users.noreply.github.com>
2023-04-09 00:19:10 +0530

    removed error

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
 streamlit.title('My parents new healthy diner')
-streamlit.head('My parents new healthy diner')
+streamlit.header('My parents new healthy diner')
 streamlit.text('My parents new healthy diner')
