f62ef77f013a9bfeeddc105d9b180244eae0c4e2
Kavi2101sh <36762897+Kavi2101sh@users.noreply.github.com>
2023-04-09 00:20:14 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,4 @@
 import streamlit
-streamlit.title('My parents new healthy diner')
-streamlit.header('My parents new healthy diner')
-streamlit.text('My parents new healthy diner')
+streamlit.title('My parents new healthy diner  🐔')
+streamlit.header('My parents new healthy diner 🥑🍞')
+streamlit.text('My parents new healthy diner 🥣 🥗 ')
