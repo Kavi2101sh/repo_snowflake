c16634d1530c1e28dc54cf80b2cc21f6d5c1f5f7
Kavi2101sh <36762897+Kavi2101sh@users.noreply.github.com>
2023-04-17 00:10:35 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,26 +13,26 @@ def get_fruit_load_list():
     my_cur.execute("select * from fruit_load_list")
     return my_cur.fetchall()
     
-streamlit.title('My parents new healthy diner  🐔')
-streamlit.header('My parents new healthy diner 🥑🍞')
-streamlit.text('My parents new healthy diner 🥣 🥗 ')
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
-fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
-streamlit.dataframe(fruits_to_show)
-streamlit.header("Fruityvice Fruit Advice!")
+# streamlit.title('My parents new healthy diner  🐔')
+#streamlit.header('My parents new healthy diner 🥑🍞')
+#streamlit.text('My parents new healthy diner 🥣 🥗 ')
+#streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+#my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+#my_fruit_list = my_fruit_list.set_index('Fruit')
+#fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
+#fruits_to_show = my_fruit_list.loc[fruits_selected]
+#streamlit.dataframe(fruits_to_show)
+#streamlit.header("Fruityvice Fruit Advice!")
 # write your own comment -what does the next line do? 
-try:
-  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-  if not fruit_choice:
-    streamlit.error("Please select a fruit to get information")
-  else:
-    back_from_function = get_fruityvice_data(fruit_choice)
-    streamlit.dataframe(back_from_function)
-except URLError as e:
-   streamlit.error()
+#try:
+#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+#  if not fruit_choice:
+#    streamlit.error("Please select a fruit to get information")
+#  else:
+#    back_from_function = get_fruityvice_data(fruit_choice)
+#    streamlit.dataframe(back_from_function)
+#except URLError as e:
+#   streamlit.error()
 # streamlit.stop()
 if streamlit.button("Get Fruit Load List"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
