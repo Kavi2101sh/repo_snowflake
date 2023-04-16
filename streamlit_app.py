a4d470b27bca9a06efd714f877915e59ccfc481f
Kavi2101sh <36762897+Kavi2101sh@users.noreply.github.com>
2023-04-17 00:14:38 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -22,17 +22,17 @@ def get_fruit_load_list(my_cnx):
 #fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Apple','Avocado'])
 #fruits_to_show = my_fruit_list.loc[fruits_selected]
 #streamlit.dataframe(fruits_to_show)
-#streamlit.header("Fruityvice Fruit Advice!")
+streamlit.header("Fruityvice Fruit Advice!")
 # write your own comment -what does the next line do? 
-#try:
-#  fruit_choice = streamlit.text_input('What fruit would you like information about?')
-#  if not fruit_choice:
-#    streamlit.error("Please select a fruit to get information")
-#  else:
-#    back_from_function = get_fruityvice_data(fruit_choice)
-#    streamlit.dataframe(back_from_function)
-#except URLError as e:
-#   streamlit.error()
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("Please select a fruit to get information")
+  else:
+    back_from_function = get_fruityvice_data(fruit_choice)
+    streamlit.dataframe(back_from_function)
+except URLError as e:
+   streamlit.error()
 # streamlit.stop()
 if streamlit.button("Get Fruit Load List"):
   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
