fd6e9808b775c3cde1825fd718d0d4339f5e776c
Kavi2101sh <36762897+Kavi2101sh@users.noreply.github.com>
2023-04-17 00:12:23 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -39,6 +39,6 @@ if streamlit.button("Get Fruit Load List"):
   my_data_rows = get_fruit_load_list(my_cnx)
   streamlit.dataframe(my_data_row)
 
-add_my_fruit = streamlit.text_input('What fruit would you like information about?')
-streamlit.write("Thanks for adding" ,add_my_fruit);
-my_cur.execute("insert into fruit_load_list values('from streamlit')");
+#add_my_fruit = streamlit.text_input('What fruit would you like information about?')
+#streamlit.write("Thanks for adding" ,add_my_fruit);
+#my_cur.execute("insert into fruit_load_list values('from streamlit')");
