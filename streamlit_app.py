de08e3fb75df76ca3444d6146b642ddde5a74e15
Kavi2101sh <36762897+Kavi2101sh@users.noreply.github.com>
2023-04-17 00:19:42 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,7 @@ def get_fruit_load_list(my_cnx):
 def insert_row_snowflake(new_fruit):
   with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values('from streamlit')")
+    return "Thanks for adding " + new_fruit
     
 # streamlit.title('My parents new healthy diner  🐔')
 #streamlit.header('My parents new healthy diner 🥑🍞')
